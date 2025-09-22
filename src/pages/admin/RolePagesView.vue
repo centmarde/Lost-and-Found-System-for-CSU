b913fcd07c127b61ca6888303c8f50ea9f086f02
BritneySpears <centmarde.campado@gmail.com>
2025-09-23 01:06:10 +0800

    feat: Add Role Management page and implement theme store with persistence

---
+++ src/pages/admin/RolePagesView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import InnerLayoutWrapper from '@/layouts/InnerLayoutWrapper.vue';
+</script>
+
+<template>
+<InnerLayoutWrapper>
+  <template #content>
+    <h1>Role Management</h1>
+  </template>
+</InnerLayoutWrapper>
+</template>
